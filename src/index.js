18eecce11de792701fd632801afd7a0ea5e7edf0
manodecristiano <cristian.carre.online@gmail.com>
2022-03-11 00:42:55 +0100

    Read Users

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import { init } from "./js/usuarios-page";
-import { obtenerUsuarios } from "./js/http-provider";
+// import { init } from "./js/usuarios-page";
+// import { obtenerUsuarios } from "./js/http-provider";
 
 
-obtenerUsuarios().then(console.log);
+// obtenerUsuarios().then(console.log);
 
 
- init();
+//  init();
 
 
+import * as CRUD  from  './js/crud-provider.js';
+
+CRUD.getUsuario( 1 ).then( console.log );
